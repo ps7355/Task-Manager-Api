0173ea58fa77fd26b52a785e334280daa9d4598b
P G SIVARANJAN <103554138+ps7355@users.noreply.github.com>
2024-02-06 18:38:07 +0530

    Update router.js

--- Routes/router.js
+++ Routes/router.js
@@ -3,6 +3,6 @@ const routes= express.Router();
 const handler = require('./../Controller/taskController')
 
 routes.route('/').post(handler.addTasks).get(handler.getAllTask);
-routes.route('/:id').delete(handler.deleteTaskById).patch(handler.editTaskById);
+routes.route('/:id').delete(handler.deleteTaskById).put(handler.editTaskById);
 
 module.exports = routes;
